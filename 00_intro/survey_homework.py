dc7be0ac99bdf780c41e554965575c68837ee7c8
Josiah Davis <josiah.j.davis@gmail.com>
2025-12-10 22:17:22 -0600

    Create survey_homework.py

---
+++ 00_intro/survey_homework.py
@@ -0,0 +1,29 @@
+participants = [
+    {"id": 0, "age": 25, "hours_exercise": 3.2, "gender": "F"},
+    {"id": 1, "age": 31, "hours_exercise": 0.0, "gender": "M"},
+    {"id": 2, "age": 44, "hours_exercise": 1.5, "gender": "F"},
+    {"id": 3, "age": 29, "hours_exercise": 2.0, "gender": "M"},
+    {"id": 4, "age": 61, "hours_exercise": 1.5, "gender": "F"},
+    {"id": 5, "age": 37, "hours_exercise": -1.0, "gender": "M"}  # Suspicious value
+]
+
+# 1. Print Participant Exercise Hours
+
+
+# 2. Replace Negative Hours With None
+
+
+# 3. Compute Average Weekly Hours
+
+
+# Expected output
+# 
+# Participant 1: 3.2 hours
+# Participant 2: 0.0 hours
+# Participant 3: 1.5 hours
+# Participant 4: 2.0 hours
+# Participant 5: 1.5 hours
+# Participant 6: -1.0 hours
+# 
+# Overall average hours: 1.64
+
